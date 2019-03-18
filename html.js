91893f21b5b6aecbc8fdc27313facdce44045031
Tam <ilovepc830@gmail.com>
2019-03-18 20:18:53 +0700

    Project Eloquent JavaScript Ebook

---
+++ html.js
@@ -0,0 +1,17 @@
+
+
+{
+	tagName: 'body',
+	children:[
+		{
+			href: 'http://google.com',
+			target: '_blank',
+			content: 'Goolge',
+			tagName: 'a'
+		},
+		{
+			content: 'A paragraph',
+			tagName: 'p'
+		}
+	]
+}
